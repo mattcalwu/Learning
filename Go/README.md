6d4a61a1382722e5c4980eb7efc05d6a6624ed17
mattcalwu <mattcalwu@gmail.com>
2024-06-01 20:27:57 -0700

    Finished go tutorial

--- Go/README.md
+++ Go/README.md
@@ -8,3 +8,60 @@ command line tool.
 
 I will be learning Go to create my own CLI-tools.
 
+### Useful commands
+
+#### Start module:
+
+```go
+go mod init <module-name>
+```
+
+Edit module to redirect:
+
+```go
+go mod edit -replace <module>=<localFile>
+```
+
+Synchronise module (adds files that are required in code):
+
+```go
+go mod tidy
+```
+
+Running the file:
+
+```go
+go run .
+```
+
+#### Running tests:
+
+```go
+go test
+go test -v
+```
+
+#### Build:
+
+```go
+go build
+```
+
+#### List:
+
+This lists where your build directory is.
+
+```go
+go list -f '{{.Target}}'
+```
+
+#### Install:
+
+This allows you to run the binary you build anywhere
+
+```go
+go install
+```
+
+
+
--- Go/greetings/greetings.go
+++ Go/greetings/greetings.go
@@ -1,58 +1,59 @@
 package greetings
 
 import (
-  "errors"
-  "fmt"
-  "math/rand"
+	"errors"
+	"fmt"
+	"math/rand"
 )
 
 // Hello returns a greeting for the named person.
 func Hello(name string) (string, error) {
-  // If no name was given, return an error with a message.
-  if name == "" {
-    return "", errors.New("empty name")
-  }
-  // A function whose name starts with a capital letter can be called by a
-  // function not in the same package, aka exported name
-  // Return a greeting that embeds the name in a message.
-  message := fmt.Sprintf(randomFormat(), name)
-  // Walrus is a way to declare and initialize a variable in one line
-  // Long way is like this: 
-  // var message string
-  // message = ...
-  return message, nil
+	// If no name was given, return an error with a message.
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+	// A function whose name starts with a capital letter can be called by a
+	// function not in the same package, aka exported name
+	// Return a greeting that embeds the name in a message.
+	message := fmt.Sprintf(randomFormat(), name)
+
+	// Walrus is a way to declare and initialize a variable in one line
+	// Long way is like this:
+	// var message string
+	// message = ...
+	return message, nil
 }
 
 // Hellos returns a map that associates each of the named people
 // with a greeting message.
 func Hellos(names []string) (map[string]string, error) {
-  // A map to associate names with messages.
-  messages := make(map[string]string)
-  // Loop through the received slice of names, calling
-  // the Hello function to get a message for each name.
-  for _, name := range names {
-    message, err := Hello(name)
-    if err != nil {
-      return nil, err
-    }
-    // In the map, associate the retrieved message with
-    // the name.
-    messages[name] = message
-  }
-  return messages, nil
+	// A map to associate names with messages.
+	messages := make(map[string]string)
+	// Loop through the received slice of names, calling
+	// the Hello function to get a message for each name.
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		// In the map, associate the retrieved message with
+		// the name.
+		messages[name] = message
+	}
+	return messages, nil
 }
 
 // randomFormat returns one of a set of greeting messages. The returned
 // message is selected at random
 func randomFormat() string {
-  // A slice of message formats
-  formats := []string{
-    "Hi, %v. Welcome!",
-    "Great to see you, %v!",
-    "Hail, %v! Well met!",
-  }
+	// A slice of message formats
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
 
-  // Return a randomly selected message format by specifying
-  // a random index for the slice of formats.
-  return formats[rand.Intn(len(formats))]
+	// Return a randomly selected message format by specifying
+	// a random index for the slice of formats.
+	return formats[rand.Intn(len(formats))]
 }
